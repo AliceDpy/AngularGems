474acd2b89203302a97da4c3854e487bbab39b89
AliceDpy <alice.depinoy@gmail.com>
2017-03-18 12:12:05 +0100

    header, advantages & best products added and start styling

--- js/app.js
+++ js/app.js
@@ -5,32 +5,46 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+
+
 var gems = [{
     name: 'Dodecahedron',
     price: 2.95,
-    description: '. . .',
+    description: 'lorem ipsum super',
     canPurchase: true,
     soldOut: true,
     images: [{
-      full:'assets/gem-01.gif',
-      thumb:'assets/gem-01.gif',
+      full:'assets/gem-03.gif',
+      thumb:'assets/gem-03.gif',
     }],
   },
   {
     name: 'Youpi',
     price: 23.10,
-    description: '. . .',
+    description: 'je suis baptiste',
     canPurchase: true,
     soldOut: true,
     images: [{
-      full:'assets/gem-02.gif',
-      thumb:'assets/gem-02.gif',
+      full:'assets/gem-03.gif',
+      thumb:'assets/gem-03.gif',
     }],
   },
   {
     name: 'Houla',
     price: 5.40,
-    description: '. . .',
+    description: 'tequila',
     canPurchase: true,
     soldOut: true,
     images: [{
